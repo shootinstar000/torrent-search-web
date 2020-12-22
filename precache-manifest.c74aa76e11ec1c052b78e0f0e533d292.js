6e5aa2fe5911836c8587ede70fb9a9d5d2073064
Tejasvp25 <tejasvp25@gmail.com>
2020-12-22 20:53:52 +0530

    Updates

---
+++ precache-manifest.c74aa76e11ec1c052b78e0f0e533d292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0a8e68bc52284c40d304baefa62795",
+    "url": "/torrent-search-web/index.html"
+  },
+  {
+    "revision": "12f1c9186f5de79592f4",
+    "url": "/torrent-search-web/static/css/main.10e099d0.chunk.css"
+  },
+  {
+    "revision": "22853ceaa98416eaa5fe",
+    "url": "/torrent-search-web/static/js/2.c5231fab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/torrent-search-web/static/js/2.c5231fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f1c9186f5de79592f4",
+    "url": "/torrent-search-web/static/js/main.74e104a6.chunk.js"
+  },
+  {
+    "revision": "1f805d2771630bab1587",
+    "url": "/torrent-search-web/static/js/runtime-main.c9d9ffb6.js"
+  }
+]);
